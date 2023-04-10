8f0e63c28e2d27e5d95c41e3abba121af2e47e76
Rhea1409 <130371639+Rhea1409@users.noreply.github.com>
2023-04-10 19:17:04 +0530

    Add files via upload

---
+++ Program 6 revised again.py
@@ -0,0 +1,5 @@
+s=int(input("Enter the side of the square:"))
+ar=s*s
+p=4*s
+print("The parameter of the square is:", p)
+print("The area of the square:", ar)
---
+++ Something.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+Sum= a+b
+product= a*b
+print("The sum is",sum)
+print("The product is",product)
+
+
